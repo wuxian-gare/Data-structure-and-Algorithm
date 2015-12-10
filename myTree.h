e386096340749ed888089fae117eb0418d0851f4
noline <511496051@qq.com>
2015-12-10 16:18:20 +0800

    Update myTree.h

--- myTree.h
+++ myTree.h
@@ -202,7 +202,9 @@ public:
 		cout << endl;
 	}
 
-
+	/*
+	*后序遍历3
+	*/
 	void postOrder_3(TreeNode *p){
 		if (!p) 
 			return;
@@ -257,5 +259,233 @@ public:
 		} 
 		cout << endl;
 	}//end of morris_inorder_traversal
+	
+	/*
+	*给定一棵树，同时给出树中的两个结点(n1和n2)，求它们的最低公共祖先。
+	*也就是常见的LCA(Lowest Common Ancestor )问题。
+	*/
+	//找到从root到 节点值为key的路径,存储在path中。没有的话返回-1
+	bool findpath(TreeNode * root, vector<int> &path, int key){
+		if (root == NULL) 
+			return false;
+		path.push_back(root->val);
+		if (root->val == key) 
+			return true;
+				//左子树或右子树 是否找到,找到的话当前节点就在路径中了
+		bool find = (findpath(root->left, path, key) || findpath(root->right, path, key));
+		if (find) 
+			return true;
+				//该节点下未找到就弹出
+		path.pop_back();
+		return false;
+	}
 
+	int findLCA(TreeNode * root, int key1, int key2){			//比较两条路径的最后一个相同节点
+		vector<int> path1, path2;
+		bool find1 = findpath(root, path1, key1);
+		bool find2 = findpath(root, path2, key2);
+		if (find1 && find2){
+			int ans;
+			for (int i = 0; i<path1.size(); i++){
+				if (path1[i] != path2[i]){
+						break;
+				}
+				else
+					ans = path1[i];
+			}
+			return ans;
+		}
+		return -1;
+	}
+
+	/*
+	*更为简便的LCA算法
+	*从root开始遍历，如果n1和n2中的任一个和root匹配，那么root就是LCA。 
+	*如果都不匹配，则分别递归左、右子树，如果有一个 key（n1或n2）出现在左子树，
+	*并且另一个key(n1或n2)出现在右子树，则root就是LCA.  如果两个key都出现在左
+	*子树，则说明LCA在左子树中，否则在右子树。
+	*/
+	/*
+	*时间复杂度为O(n)，但是此方法还是有所局限的，必须保证两个要查找的节点n1和n2都出现在树中。
+	*如果n1不在树中，则会返回n2为LCA，理想答案应该为NULL。要解决这个问题，可以先查找下 n1和n2是否
+	*出现在树中，然后加几个判断即可。
+	*/
+	struct TreeNode *findLCA2(struct TreeNode* root, int n1, int n2){
+		if (root == NULL) return NULL;
+			// 只要n1 或 n2 的任一个匹配即可
+			//  (注意：如果 一个节点是另一个祖先，则返回的是祖先节点。因为递归是要返回到祖先的 )
+		if (root->val == n1 || root->val == n2)
+			return root;
+			// 分别在左右子树查找
+		TreeNode *left_lca = findLCA2(root->left, n1, n2);
+		TreeNode *right_lca = findLCA2(root->right, n1, n2);
+			// 如果都返回非空指针 Non-NULL, 则说明两个节点分别出现了在两个子树中，则当前节点肯定为LCA
+		if (left_lca && right_lca)  
+			return root;
+			// 如果一个为空，在说明LCA在另一个子树
+		return (left_lca != NULL) ? left_lca : right_lca;
+	}
+
+	/*
+	* Leetcode #96:	Unique Binary Search Trees
+	* Given n, how many structurally unique BST's (binary search trees) that store values 1...n?
+	*/
+	int numTrees(int n) {
+		if (n<1)
+			return 0;
+		int pre, next;
+		int sum;
+		vector<int> res;
+		res.push_back(1);
+		res.push_back(1);
+		res.push_back(2);
+		for (int i = 3; i <= n; i++){
+			sum = 0;
+			for (int j = 1; j <= i; j++){
+				pre = j - 1;
+				next = i - j;
+				sum += res[pre] * res[next];
+			}
+			res.push_back(sum);
+		}
+		return res[n];
+
+	}
+
+	/*
+	* Leetcode #108:	Convert Sorted Array to Binary Search Tree
+	*/
+	TreeNode* sortedArrayToBST(vector<int>& nums) {
+		if (nums.size() == 0)
+			return NULL;
+		int mid = nums.size() / 2;
+		TreeNode *root = new TreeNode(nums[mid]);
+		cout << "Node is :" << nums[mid] << endl;
+		if (mid == 0){
+			root->left = NULL;
+			root->right = NULL;
+			return root;
+		}
+		vector<int> left;
+		vector<int> right;
+		left.insert(left.begin(), nums.begin(), nums.begin() + mid);
+		right.insert(right.begin(), nums.begin() + mid + 1, nums.end());
+		root->left = sortedArrayToBST(left);
+		root->right = sortedArrayToBST(right);
+		return root;
+	}
+
+	void flatten(TreeNode* root) {
+		if (root == NULL)
+			return;
+		TreeNode *start = root;
+		TreeNode *temp;
+		while (1){
+			if (start->left != NULL&&start->right != NULL){
+				temp = start->left;
+				while (temp->right != NULL)
+					temp = temp->right;
+				temp->right = start->right;
+				start->right = start->left;
+				start->left = NULL;
+				start = start->right;
+			}
+			else if (start->left != NULL&&start->right == NULL){
+				start->right = start->left;
+				start->left = NULL;
+				start = start->right;
+			}
+			else if (start->left == NULL&&start->right != NULL){
+				start = start->right;
+			}
+			else{
+				break;
+			}
+		}
+	}
+
+	/*
+	* Leetcode #111: Minimum Depth of Binary Tree
+	* Given a binary tree, find its minimum depth.
+	*
+	* The minimum depth is the number of nodes along the shortest path from
+	* the root node down to the nearest leaf node.
+	*/
+	int minDepth(TreeNode* root) {
+		if (root == NULL)
+			return 0;
+		if (root->left == NULL&&root->right == NULL)			//叶子节点
+			return 1;
+		if (root->left != NULL&&root->right == NULL)			//只有右子树
+			return minDepth(root->left) + 1;
+		if (root->left == NULL&&root->right != NULL)			//只有左子树
+			return minDepth(root->right) + 1;
+		return min(minDepth(root->left), minDepth(root->right)) + 1;
+	}
+
+	/*
+	* Leetcode #129：Sum Root to Leaf Numbers
+	* 将从root到所有叶子节点的序列组成数累加起来
+	* 使用了递归
+	*/
+	int sumNumbers(TreeNode* root) {
+		if (root == NULL)
+			return 0;
+		int res = 0;
+		int sum = 0;
+		addToLeaf(root, res, sum);
+		return res;
+	}
+	void addToLeaf(TreeNode *root, int &res, int sum){
+		if (NULL == root)
+			return;
+		if (NULL == root->left&&NULL == root->right){
+			res += (sum * 10 + root->val);
+			return;
+		}
+		else{
+			sum = sum * 10 + root->val;
+			addToLeaf(root->left, res, sum);
+			addToLeaf(root->right, res, sum);
+		}
+	}
+
+	/*
+	* Leetcode #199：Binary Tree Right Side View
+	* 返回一棵树的右视图
+	* 使用一个vector<TreeNode *> Node存下每层的所有节点
+	*/
+	vector<int> rightSideView(TreeNode* root) {
+		vector<int> res;
+		if (root == NULL)
+			return res;
+		TreeNode* start = root;
+		res.push_back(root->val);
+		vector<TreeNode *> Node;
+		Node.push_back(root);
+		int size = 0;
+		while (1){
+			size = Node.size();
+			for (int i = 0; i<size; i++){
+				if (Node[0]->left != NULL)
+					Node.push_back(Node[0]->left);
+				if (Node[0]->right != NULL)
+					Node.push_back(Node[0]->right);
+				Node.erase(Node.begin());
+			}
+			if (Node.empty())
+				return res;
+			res.push_back(Node[Node.size() - 1]->val);		//存入最右边的数
+		}
+	}
+
+	/*max()  min()函数*/
+	int max(int a, int b){
+		return a > b ? a : b;
+	}
+
+	int min(int a, int b){
+		return a < b ? a : b;
+	}
+	/*end of class myTree*/
 }
