4885e79f5b22f86922ba67a0615e5f262ba802a3
noline <511496051@qq.com>
2015-12-10 16:05:21 +0800

    Create myHead.h

---
+++ myHead.h
@@ -0,0 +1,14 @@
+/*
+*自建头文件
+*/
+
+#include <iostream>
+#include <set>
+#include <string>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <Algorithm>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
