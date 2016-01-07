5fb9a3967ee0bd4a820806e1d835ee2f5717a158
Wu Xian <511496051@qq.com>
2016-01-07 13:33:07 +0800

    Update myVector.h
    add function IsPopOrder()

--- myVector.h
+++ myVector.h
@@ -673,7 +673,31 @@ int minNumberInRotateArray(vector<int> rotateArray) {
 		}
 		return result;
 	}
-
+	/*
+	* 输入两个整数序列，第一个序列表示栈的压入顺序，请判断第二个序列是否为该栈的弹出顺序。
+	* 假设压入栈的所有数字均不相等。例如序列1,2,3,4,5是某栈的压入顺序，序列4，5,3,2,1是该
+	* 压栈序列对应的一个弹出序列，但4,3,5,1,2就不可能是该压栈序列的弹出序列。
+	*/
+        bool IsPopOrder(vector<int> pushV,vector<int> popV) {
+        	stack<int> res;
+        	int offset=0;
+        	int pos=0;
+        	while(1){
+        	    if(res.empty()||res.top()!=popV[offset]){
+        	       if(pushV.size()==pos)
+        	            return false;
+                	res.push(pushV[pos]);
+                	pos++;
+                	continue;
+            	    }
+            	    else{
+                  	    offset++;
+                	    if(offset==popV.size())
+                    		    return true;
+                	    res.pop();
+            	    }
+        	}	
+    	}
 
 
 
